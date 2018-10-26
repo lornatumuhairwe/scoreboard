537ae65142d61d6254ba8a74916f9a3aac312ff0
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2018-10-26 13:33:45 +0300

    Arrow function Component with explicit return.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const Header = () => (
-    <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
-    </header>
-);
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+};
 
-ReactDOM.render(
+ReactDOM.render (
     < Header />,
     document.getElementById('root')
 );
