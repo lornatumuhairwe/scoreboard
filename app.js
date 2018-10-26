75c8bab71c8b6577146128dba7a6246a82c365e2
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2018-10-26 13:29:02 +0300

    React component with a function.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const desc = 'I just learned to create a React node and render it to the DOM.';
-const myTitleID = 'main-title';
-const name = 'Lorna';
-
-const header = (
-    <header>
-        <h1 id={ myTitleID }>{ name }'s first React Element!</h1>
-        <p className="main-desc">{ desc }</p>
-    </header>
-);
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
-    header,
+    < Header />,
     document.getElementById('root')
 );
